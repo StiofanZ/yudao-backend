2b7ecc440c371cd37e9476abb4bf60edc3151bc9
zhf <zhf@63aaa323-bd20-4782-bdcf-f624a29d9816>
2025-12-31 08:13:41 +0000

    workflow finish wf-sq-tfsq
    git-svn-id: svn://172.16.27.209/gsgh/gsgh/lghjft-backend@499 63aaa323-bd20-4782-bdcf-f624a29d9816

--- yudao-module-lghjft/src/main/java/cn/iocoder/yudao/module/lghjft/dal/dataobject/workflow/wfsqtfsq/WfSqTfsqmxDO.java
+++ yudao-module-lghjft/src/main/java/cn/iocoder/yudao/module/lghjft/dal/dataobject/workflow/wfsqtfsq/WfSqTfsqmxDO.java
@@ -1,3 +1,5 @@
+package cn.iocoder.yudao.module.lghjft.dal.dataobject.workflow.wfsqtfsq;
+
 import cn.iocoder.yudao.framework.mybatis.core.dataobject.BaseDO;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.KeySequence;
