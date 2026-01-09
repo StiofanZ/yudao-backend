0ea033e09599428e0740be3c2f4f2566fdf05ed9
wxc <wxc@63aaa323-bd20-4782-bdcf-f624a29d9816>
2026-01-09 00:54:24 +0000

    重复文件
    git-svn-id: svn://172.16.27.209/gsgh/gsgh/lghjft-backend@520 63aaa323-bd20-4782-bdcf-f624a29d9816

--- yudao-module-lghjft/src/main/java/cn/iocoder/yudao/module/dm/enums/ErrorCodeConstants_手动操作.java
+++
@@ -1,8 +0,0 @@
-//// TODO 待办：请将下面的错误码复制到 yudao-module-dm 模块的 ErrorCodeConstants 类中。注意，请给“TODO 补充编号”设置一个错误码编号！！！
-//// ========== 行政区划 TODO 补充编号 ==========
-//ErrorCode XZQH_NOT_EXISTS = new ErrorCode(TODO 补充编号, "行政区划不存在");
-//ErrorCode XZQH_EXITS_CHILDREN = new ErrorCode(TODO 补充编号, "存在存在子行政区划，无法删除");
-//ErrorCode XZQH_PARENT_NOT_EXITS = new ErrorCode(TODO 补充编号,"父级行政区划不存在");
-//ErrorCode XZQH_PARENT_ERROR = new ErrorCode(TODO 补充编号, "不能设置自己为父行政区划");
-//ErrorCode XZQH_XZQHMC_DUPLICATE = new ErrorCode(TODO 补充编号, "已经存在该行政区划名称的行政区划");
-//ErrorCode XZQH_PARENT_IS_CHILD = new ErrorCode(TODO 补充编号, "不能设置自己的子Xzqh为父Xzqh");
