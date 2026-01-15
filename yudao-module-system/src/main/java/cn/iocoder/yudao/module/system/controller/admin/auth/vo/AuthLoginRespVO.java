dda67be084276286cb29a5d972e7e8d51c816d6f
spj <spj@63aaa323-bd20-4782-bdcf-f624a29d9816>
2026-01-15 02:08:16 +0000

    用户登录增加：用户昵称、性别、头像、部门
    git-svn-id: svn://172.16.27.209/gsgh/gsgh/lghjft-backend@543 63aaa323-bd20-4782-bdcf-f624a29d9816

--- yudao-module-system/src/main/java/cn/iocoder/yudao/module/system/controller/admin/auth/vo/AuthLoginRespVO.java
+++ yudao-module-system/src/main/java/cn/iocoder/yudao/module/system/controller/admin/auth/vo/AuthLoginRespVO.java
@@ -18,6 +18,18 @@ public class AuthLoginRespVO {
     @Schema(description = "用户编号", requiredMode = Schema.RequiredMode.REQUIRED, example = "1024")
     private Long userId;
 
+    @Schema(description = "用户账号", requiredMode = Schema.RequiredMode.REQUIRED, example = "tom")
+    private String userName;
+
+    @Schema(description = "用户昵称", requiredMode = Schema.RequiredMode.REQUIRED, example = "张三")
+    private String nickName;
+
+    @Schema(description = "用户头像", requiredMode = Schema.RequiredMode.REQUIRED, example = "http://test.yudao.iocoder.cn/20251216/blob_1765819846641.png")
+    private String avatar;
+
+    @Schema(description = "部分ID", requiredMode = Schema.RequiredMode.REQUIRED, example = "620100")
+    private Long deptId;
+
     @Schema(description = "访问令牌", requiredMode = Schema.RequiredMode.REQUIRED, example = "happy")
     private String accessToken;
 
