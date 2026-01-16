8724c0fa5e6ba5e236cdf8c90a8307a90b22fa05
wxc <wxc@63aaa323-bd20-4782-bdcf-f624a29d9816>
2026-01-16 04:06:27 +0000

    AutoConfiguration自动装配
    git-svn-id: svn://172.16.27.209/gsgh/gsgh/lghjft-backend@550 63aaa323-bd20-4782-bdcf-f624a29d9816

--- yudao-module-lghjft/src/main/java/cn/iocoder/yudao/module/lghjft/framework/auth/config/LghJftAuthConfiguration.java
+++ yudao-module-lghjft/src/main/java/cn/iocoder/yudao/module/lghjft/framework/auth/config/LghJftAuthConfiguration.java
@@ -1,10 +1,24 @@
 package cn.iocoder.yudao.module.lghjft.framework.auth.config;
 
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@AutoConfiguration
 @EnableConfigurationProperties({LghJftAuthProperties.class,
                                 LghJftAppAuthProperties.class})
 public class LghJftAuthConfiguration {
-}
+
+
+    /**
+     * 验证配置是否加载成功
+     */
+    @Bean
+    public String lghJftConfigValidator(LghJftAuthProperties authProps,
+                                        LghJftAppAuthProperties appProps) {
+        System.out.println("LghJft 配置加载成功:");
+        System.out.println("  - Auth AppId: " + authProps.getAppId());
+        System.out.println("  - App AppId: " + appProps.getAppId());
+        return "LghJft Config Ready";
+    }}
