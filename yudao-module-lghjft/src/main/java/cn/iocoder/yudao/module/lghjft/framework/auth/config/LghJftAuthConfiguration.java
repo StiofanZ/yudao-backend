876a5ab486d3c7b0298ce8cc320e663f5f3764ca
wxc <wxc@63aaa323-bd20-4782-bdcf-f624a29d9816>
2026-01-16 01:46:17 +0000

    单点登录local配置
    git-svn-id: svn://172.16.27.209/gsgh/gsgh/lghjft-backend@549 63aaa323-bd20-4782-bdcf-f624a29d9816

--- yudao-module-lghjft/src/main/java/cn/iocoder/yudao/module/lghjft/framework/auth/config/LghJftAuthConfiguration.java
+++ yudao-module-lghjft/src/main/java/cn/iocoder/yudao/module/lghjft/framework/auth/config/LghJftAuthConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties(LghJftAuthProperties.class)
+@EnableConfigurationProperties({LghJftAuthProperties.class,
+                                LghJftAppAuthProperties.class})
 public class LghJftAuthConfiguration {
 }
