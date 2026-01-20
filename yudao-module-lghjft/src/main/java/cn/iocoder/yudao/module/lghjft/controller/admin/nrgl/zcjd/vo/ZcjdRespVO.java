3798eb700b47f81e64e0e21d55ea81efb2fbf670
spj <spj@63aaa323-bd20-4782-bdcf-f624a29d9816>
2026-01-20 07:27:41 +0000

    政策法规增加相关字段
    git-svn-id: svn://172.16.27.209/gsgh/gsgh/lghjft-backend@566 63aaa323-bd20-4782-bdcf-f624a29d9816

--- yudao-module-lghjft/src/main/java/cn/iocoder/yudao/module/lghjft/controller/admin/nrgl/zcjd/vo/ZcjdRespVO.java
+++ yudao-module-lghjft/src/main/java/cn/iocoder/yudao/module/lghjft/controller/admin/nrgl/zcjd/vo/ZcjdRespVO.java
@@ -1,5 +1,6 @@
 package cn.iocoder.yudao.module.lghjft.controller.admin.nrgl.zcjd.vo;
 
+import com.baomidou.mybatisplus.annotation.TableId;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import java.time.LocalDateTime;
@@ -10,16 +11,86 @@ import java.time.LocalDateTime;
 @ToString(callSuper = true)
 public class ZcjdRespVO extends ZcjdBaseVO {
 
+    /**
+     * 编号
+     */
     @Schema(description = "编号", requiredMode = Schema.RequiredMode.REQUIRED, example = "1024")
     private Long id;
 
-    @Schema(description = "创建时间", requiredMode = Schema.RequiredMode.REQUIRED)
-    private LocalDateTime createTime;
+    /**
+     * 父编号
+     */
+    @Schema(description = "父编号", requiredMode = Schema.RequiredMode.REQUIRED, example = "1024")
+    private Long parentId;
 
-    @Schema(description = "部门编号", requiredMode = Schema.RequiredMode.REQUIRED, example = "10")
+    /**
+     * 标题
+     */
+    @Schema(description = "标题", requiredMode = Schema.RequiredMode.REQUIRED, example = "1024")
+    private String title;
+
+    /**
+     * 内容
+     */
+    @Schema(description = "内容", requiredMode = Schema.RequiredMode.REQUIRED, example = "1024")
+    private String content;
+
+    /**
+     * 排序
+     */
+    @Schema(description = "排序", requiredMode = Schema.RequiredMode.REQUIRED, example = "1024")
+    private Integer sort;
+
+    /**
+     * 状态
+     * 0: 未审核, 1: 已审核, 2: 已发布, 3: 已过期, 4: 已下架
+     */
+    @Schema(description = "状态", requiredMode = Schema.RequiredMode.REQUIRED, example = "1024")
+    private Integer status;
+
+    /**
+     * 发布部门编号
+     */
+    @Schema(description = "发布部门编号", requiredMode = Schema.RequiredMode.REQUIRED, example = "1024")
     private Long deptId;
 
-    @Schema(description = "部门名称", example = "研发部")
     private String deptName;
+
+    /**
+     * 可见范围
+     * 1: 完全可见, 2: 下级可见, 3: 本级可见
+     */
+    @Schema(description = "可见范围", requiredMode = Schema.RequiredMode.REQUIRED, example = "1024")
+    private Integer kjfw;
+
+    /**
+     * 附件路径
+     */
+    @Schema(description = "附件路径", requiredMode = Schema.RequiredMode.REQUIRED, example = "1024")
+    private String fjlj;
+
+    /**
+     * 发布部门(0:全总,1:省总,2:市州)
+     */
+    @Schema(description = "发布部门(0:全总,1:省总,2:市州)", requiredMode = Schema.RequiredMode.REQUIRED, example = "1024")
+    private Integer fbbm;
+
+    /**
+     * 下架原因(1:已失效政策,2:新政策替代)
+     */
+    @Schema(description = "下架原因(1:已失效政策,2:新政策替代)", requiredMode = Schema.RequiredMode.REQUIRED, example = "1024")
+    private String xjyy;
+
+    /**
+     * 原文件发布日期
+     */
+    @Schema(description = "原文件发布日期", requiredMode = Schema.RequiredMode.REQUIRED, example = "1024")
+    private java.time.LocalDateTime fbrq;
+
+    /**
+     * 关联政策ID
+     */
+    @Schema(description = "关联政策ID", requiredMode = Schema.RequiredMode.REQUIRED, example = "1024")
+    private Long glzcId;
 
 }
