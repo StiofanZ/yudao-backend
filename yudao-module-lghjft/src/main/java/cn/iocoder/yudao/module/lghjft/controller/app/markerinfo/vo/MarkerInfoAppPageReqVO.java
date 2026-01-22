6fb06cce9f86fcd62cc83bb0cc29981b5dcf103c
wxc <wxc@63aaa323-bd20-4782-bdcf-f624a29d9816>
2026-01-22 09:42:32 +0000

    办事地图app接口
    git-svn-id: svn://172.16.27.209/gsgh/gsgh/lghjft-backend@586 63aaa323-bd20-4782-bdcf-f624a29d9816

---
+++ yudao-module-lghjft/src/main/java/cn/iocoder/yudao/module/lghjft/controller/app/markerinfo/vo/MarkerInfoAppPageReqVO.java
@@ -0,0 +1,13 @@
+package cn.iocoder.yudao.module.lghjft.controller.app.markerinfo.vo;
+
+import cn.iocoder.yudao.module.lghjft.controller.admin.markerinfo.vo.MarkerInfoPageReqVO;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+@Schema(description = "用户 App- 高德地图标注点信息分页 Request VO")
+@Data
+public class MarkerInfoAppPageReqVO extends MarkerInfoPageReqVO {
+
+
+
+}
