e50796dc3fa9b5e41c7ea8eb55b312d6214db5a9
wxc <wxc@63aaa323-bd20-4782-bdcf-f624a29d9816>
2026-02-02 09:12:42 +0000

    git-svn-id: svn://172.16.27.209/gsgh/gsgh/lghjft-backend@663 63aaa323-bd20-4782-bdcf-f624a29d9816

--- yudao-module-lghjft/src/main/java/cn/iocoder/yudao/module/lghjft/controller/app/workflow/wftdfsq/WfTdfSqAppController.java
+++ yudao-module-lghjft/src/main/java/cn/iocoder/yudao/module/lghjft/controller/app/workflow/wftdfsq/WfTdfSqAppController.java
@@ -2,7 +2,7 @@ package cn.iocoder.yudao.module.lghjft.controller.app.workflow.wftdfsq;
 
 import cn.iocoder.yudao.framework.common.pojo.CommonResult;
 import cn.iocoder.yudao.module.lghjft.controller.admin.wftdfsq.vo.WfTdfSqRespVO;
-import cn.iocoder.yudao.module.lghjft.controller.app.workflow.wftdfsq.vo.WfTdfSqSaveReqVO;
+import cn.iocoder.yudao.module.lghjft.controller.admin.wftdfsq.vo.WfTdfSqSaveReqVO;
 import cn.iocoder.yudao.module.lghjft.service.wftdfsq.WfTdfSqService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -11,14 +11,13 @@ import jakarta.validation.Valid;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
 import static cn.iocoder.yudao.framework.common.pojo.CommonResult.success;
 
-@Tag(name = "用户App - 工会经费退抵费申请")
+@Tag(name = "管理后台 - 工会经费退抵费申请")
 @RestController
 @RequestMapping("/lghjft/workflow/wftdfsq")
 @Validated
-public class WfTdfSqController {
+public class WfTdfSqAppController {
 
     @Resource
     private WfTdfSqService wfTdfSqService;
@@ -35,5 +34,6 @@ public class WfTdfSqController {
     public CommonResult<Long> create(@Valid @RequestBody WfTdfSqSaveReqVO req) {
         return success(wfTdfSqService.create(req));
     }
+
 
 }
